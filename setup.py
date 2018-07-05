f998376d4c6562d0b9ca22a1a7c9c397ae0d7abd
Ian Clark <ian@ianleeclark.com>
2018-07-05 18:12:25 -0500

    Renaming to tap_facebook_reviews

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@
 from setuptools import setup
 
 setup(
-    name="tap_facebook",
+    name="tap_facebook_reviews",
     version="0.1.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["tap_facebook"],
+    py_modules=["tap_facebook_reviews"],
     install_requires=[
         "singer-python>=5.0.12",
         "requests",
     ],
     entry_points="""
     [console_scripts]
-    tap_facebook=tap_facebook:main
+    tap_facebook=tap_facebook_reviews:main
     """,
-    packages=["tap_facebook"],
+    packages=["tap_facebook_reviews"],
     package_data = {
-        "schemas": ["tap_facebook/schemas/*.json"]
+        "schemas": ["tap_facebook_reviews/schemas/*.json"]
     },
     include_package_data=True,
 )
